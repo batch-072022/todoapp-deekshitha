47de9e5bcfee8a44c2d008f2964aa431c5b8a5e1
Deekshitha Kishtamgari <deekshitha.kishtamgari@valuemomentum.com>
2022-08-02 10:32:34 +0530

    commiting my workspace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AddtaskComponent } from './addtask/addtask.component';
+import { EdittaskComponent } from './edittask/edittask.component';
+import { ListtaskComponent } from './listtask/listtask.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ListUsersComponent,
+    HeaderComponent,
+    HomeComponent,
+    AddtaskComponent,
+    EdittaskComponent,
+    ListtaskComponent
   ],
   imports: [
     BrowserModule,
